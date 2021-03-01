a08a08048217105612222174472730318339d8d7
MicroProgramer <microprogramer@yahoo.com>
2021-03-01 19:45:20 +0500

    Initial commit

---
+++ app/src/main/java/com/microprogramers/permissionasker/MainActivity.java
@@ -0,0 +1,26 @@
+package com.microprogramers.permissionasker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+
+import com.microprogramers.library.PermissionAsker;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void perm(View view)
+    {
+        String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_SMS};
+        PermissionAsker.getUserPermissions(MainActivity.this, permissions);
+    }
+}
---
+++ library/src/main/java/com/microprogramers/library/PermissionAsker.java
@@ -0,0 +1,47 @@
+package com.microprogramers.library;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+
+public class PermissionAsker {
+
+
+    public static int PERMISSION_ALL = 1;
+
+
+
+//        Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+
+
+
+
+    public static void getUserPermissions(Context context, String[] PERMISSIONS)
+    {
+        if (!hasPermissions(context, PERMISSIONS))
+        {
+            ActivityCompat.requestPermissions((Activity) context, PERMISSIONS, PERMISSION_ALL);
+        }
+    }
+
+
+    private static boolean hasPermissions(Context context, String... permissions)
+    {
+        if (context != null && permissions != null)
+        {
+            for (String permission : permissions)
+            {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
